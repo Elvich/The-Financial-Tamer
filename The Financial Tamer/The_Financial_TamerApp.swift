cde2bc78ae7247ea51679f022f395e9ae8b6f428
Maksim Gritsuk <megahappy@bk.ru>
2025-06-06 18:10:24 +0300

    Сделал основной экран приложения

--- The Financial Tamer/The_Financial_TamerApp.swift
+++ The Financial Tamer/The_Financial_TamerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct The_Financial_TamerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
