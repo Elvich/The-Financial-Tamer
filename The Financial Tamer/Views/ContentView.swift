b106df0a5d21e7f2e44c2c62c06bcc51687b8482
Maksim Gritsuk <megahappy@bk.ru>
2025-06-21 03:55:06 +0300

    Дал иконке расходов более подходящее название

--- The Financial Tamer/Views/ContentView.swift
+++ The Financial Tamer/Views/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            Tab("Расходы", image: "Expenses") {
+            Tab("Расходы", image: "Outcome") {
                 TransactionsListView(direction: .outcome)
             }
 
