b382e4d0ceca1d2c680888264b69372967aafc2c
Maksim Gritsuk <megahappy@bk.ru>
2025-06-21 03:55:59 +0300

    Создал TransactionsView

---
+++ The Financial Tamer/Views/TransactionsView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionsView.swift
+//  The Financial Tamer
+//
+//  Created by Maksim Gritsuk on 21.06.2025.
+//
+
+import SwiftUI
+
+struct TransactionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionsView()
+}
