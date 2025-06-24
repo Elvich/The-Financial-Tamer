df1551bea21ceabe6bbcb30fbbaf05c66703e3eb
Maksim Gritsuk <megahappy@bk.ru>
2025-06-24 12:52:18 +0300

    Избавился от лишней перегрузки transactionsSection

--- The Financial Tamer/Views/TransactionsView.swift
+++ The Financial Tamer/Views/TransactionsView.swift
@@ -26,29 +26,10 @@ struct TransactionsView {
         }
     }
     
-    @ViewBuilder
-    func transactionsSection() -> some View {
-        
-        let startDate: Date = dateService.startOfDay()
-        let endDate: Date = dateService.endOfDay()
-        
-        let transactions: [Transaction] = transactionService.getTransactions(start: startDate, end: endDate, direction: direction)
-        
-        Section(header: Text("Операции")) {
-            ForEach(transactions, id: \.self) { transition in
-                NavigationLink(destination: ErrorView()) {
-                    HStack {
-                        Text("\(transition.category.emoji)    \(transition.category.name)")
-                        Spacer()
-                        Text("\(transition.amount) $")
-                    }
-                }
-            }
-        }
-    }
+    
     
     @ViewBuilder
-    func transactionsSection(startDate: Date, endDate: Date, sortType: HistoryView.SortType) -> some View {
+    func transactionsSection(startDate: Date = DateService().startOfDay(), endDate: Date =  DateService().endOfDay(), sortType: HistoryView.SortType = .date) -> some View {
         let transactions: [Transaction] = transactionService.getTransactions(start: startDate, end: endDate, direction: direction)
         
         let sortedTransactions = sortTransactions(transactions, sortType)
