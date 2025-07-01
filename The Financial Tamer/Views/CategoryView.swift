ab10d2c79e29fa561648b4eb1e1560fe69cf78b3
Maksim Gritsuk <megahappy@bk.ru>
2025-07-01 09:33:40 +0300

    Добавил поиск

--- The Financial Tamer/Views/CategoryView.swift
+++ The Financial Tamer/Views/CategoryView.swift
@@ -9,20 +9,31 @@ import SwiftUI
 
 struct CategoryView: View {
     @State private var categories: [Category] = []
+    @State private var searchText: String = ""
     private let service = CategoriesService()
+    
+    var filteredCategories: [Category] {
+        if searchText.isEmpty {
+            return categories
+        } else {
+            return categories.filter { category in
+                category.name.localizedCaseInsensitiveContains(searchText) ||
+                String(category.emoji).localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            List(categories, id: \ .id) { category in
+            List(filteredCategories, id: \ .id) { category in
                 HStack {
                     Text(String(category.emoji))
-                        
                     Text(category.name)
-                        
                 }
             }
             .padding(.bottom)
             .navigationTitle("Мои статьи")
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Поиск по названию или эмодзи")
             .task {
                 categories = await service.categories()
             }
