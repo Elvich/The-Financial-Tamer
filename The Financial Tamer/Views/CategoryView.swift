d5d21856390dbf83d1f20939eb9737e2dd8bb3e3
Maksim Gritsuk <megahappy@bk.ru>
2025-07-01 09:26:14 +0300

    Создал  CategoryView

---
+++ The Financial Tamer/Views/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  The Financial Tamer
+//
+//  Created by Maksim Gritsuk on 01.07.2025.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
